d151be6893cce58bee156aeb39b308cfe31f9e5b
root <root@home.diogogomes.com>
2016-11-26 23:40:11 +0000

    license

--- homeGW.h
+++ homeGW.h
@@ -1,3 +1,23 @@
+/*
+  Written by Diogo Gomes, diogogomes@gmail.com
+
+  Copyright (c) 2014 Diogo Gomes.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 #ifndef homeGW_h
 #define homeGW_h
 
@@ -8,7 +28,7 @@
 #endif
 
 #define END_PACKET	5000
-#define MIN_PACKET	250
+#define MIN_PACKET	250	
 #define MAX_PLUGINS	2
 
 typedef void (* PacketParser) (int, unsigned int*);
