b52581a97d31efec1e19f4651794a9f611949aa0
Diogo Gomes <dgomes@av.it.pt>
2017-11-04 14:02:03 +0000

    General plugin

---
+++ plugin.cpp
@@ -0,0 +1,74 @@
+/*
+  Written by Diogo Gomes, diogogomes@gmail.com
+
+  Copyright (c) 2014 Diogo Gomes.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+#include <Plugin.h>
+
+Plugin::Plugin() {
+	uint64_t packet = 0;
+	uint8_t error = OK;
+	String error_str = "";
+}
+
+Plugin::~Plugin() {
+
+}
+
+void Plugin::detectPacket(unsigned int duration, Plugin *self ) {
+
+	if(duration > END_PACKET) {
+    	if(bitsRead > packet_size-packet_size*0.1 && bitsRead < packet_size+packet_size*0.1) {  //check if we are in the range +- 10%
+			#ifdef DEBUG
+			Serial.println(bitsRead);
+			#endif
+			self->processPacket();
+        }
+		bitsRead = 0;	
+    }
+
+	if(duration > MIN_PACKET) {
+       timings[bitsRead] = duration;
+       bitsRead++;
+	}
+
+	if(bitsRead == MAX_CHANGES) {
+    	bitsRead = 0;
+	}
+
+}
+
+
+uint64_t Plugin::getPacket() {
+  uint64_t p = packet;
+  packet = 0;
+  return p;
+}
+
+bool Plugin::available() {
+  if(Plugin::packet != 0) {
+    if(error == OK)
+      return true;
+  }
+  return false;
+}
+
+uint8_t Plugin::getErrno() {
+  return error;
+}
+
---
+++ plugin.h
@@ -0,0 +1,63 @@
+/*
+  Written by Diogo Gomes, diogogomes@gmail.com
+
+  Copyright (c) 2014 Diogo Gomes.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+#ifndef plugin_h
+#define plugin_h
+
+#if ARDUINO < 100
+  #include <WProgram.h>
+#else
+  #include <Arduino.h>
+#endif
+		
+#define MAX_CHANGES 128
+#define OK	0
+
+class Plugin {
+
+	public:
+		unsigned int timings[MAX_CHANGES];
+		unsigned int bitsRead;
+		unsigned int packet_size;
+		
+        unsigned int END_PACKET; 
+		unsigned int MIN_PACKET;
+
+		uint64_t packet;
+
+		uint8_t error;
+		String error_str;
+
+		Plugin();
+		virtual ~Plugin();
+
+		void detectPacket(unsigned int, Plugin *);
+
+		virtual void processPacket() = 0;
+
+		uint64_t getPacket();
+
+		bool available();
+
+		uint8_t getErrno();
+		String getError();
+};
+
+#endif
