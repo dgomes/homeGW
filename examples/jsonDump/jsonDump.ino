610af2e331ccc1789d21eac5f18992d8f57ccebc
Diogo Gomes <dgomes@av.it.pt>
2017-11-04 16:27:55 +0000

    new example due to refactor

--- examples/jsonDump/jsonDump.ino
+++ examples/jsonDump/jsonDump.ino
@@ -1,76 +1,85 @@
-#include "homeGW.h"
-#include "weather.h"
-#include "door.h"
-
 #define DEBUG
-#define RF_RECEIVER_PIN 3
 
-homeGW gw;
-weather station(0x3F);
-door front(0x7F7B04);
+#include <ArduinoJson.h>
+
+#include <homeGW.h>
+#include <digoo.h>
+#include <weather.h>
+#include <generic.h>
 
 
-void setup()
-{
-  Serial.begin( 115200 );   //using the serial port at 115200bps for debugging and logging
+HomeGW gw(3);
+weather station1;
+digoo station2;
+generic remote;
 
-  gw.setup(RF_RECEIVER_PIN, 0x3F);
+#define RF_RECEIVER_PIN 2
 
-  gw.registerPlugin(72, weather::detectPacket);
-  gw.registerPlugin(48, door::detectPacket);
+void setup() {
+	Serial.begin(115200);
+
+	pinMode(RF_RECEIVER_PIN, OUTPUT);
+	digitalWrite(RF_RECEIVER_PIN, LOW);
+	pinMode(RF_RECEIVER_PIN, INPUT);
+	digitalWrite(RF_RECEIVER_PIN, LOW);
+	gw.setup(RF_RECEIVER_PIN);
+
+	gw.registerPlugin(&station1); 
+	gw.registerPlugin(&station2); 
+	gw.registerPlugin(&remote); 
 }
 
-void loop()
-{
-  delayMicroseconds(1000);
+void loop() { 
+	uint64_t p = 0;
+	String output = "";
 
-  if(station.available()) {
-    uint64_t p = 0;
-    if((p = station.getPacket())) {
-      int r = weather::isValidWeather(p);
+	if(station1.available()) {
+		if((p = station1.getPacket())) {
+			StaticJsonBuffer<160> jsonBuffer;
 
-	  switch(r) {
-		case OK:
-	        Serial.print("{\"id\": \"weather\", \"code\": 200, \"Humidity\": ");
-			Serial.print(weather::getHumidity(p));
-			Serial.print(", \"Temperature\": ");
-			Serial.print(weather::getTemperature(p));
-#ifdef DEBUG
-    	    Serial.print(String(", \"Channel\": ") + weather::getChannel(p));
-#endif
-			break;
-		case INVALID_HUMIDITY:
-      		Serial.print("{\"id\": \"weather\", \"code\": 500, \"Humidity\": " + station.getError());
-			break;
-		case INVALID_TEMPERATURE:
-      		Serial.print("{\"id\": \"weather\", \"code\": 500, \"Temperature\": " + station.getError());
-			break;
-		case INVALID_SYNC:
-      		Serial.print("{\"id\": \"weather\", \"code\": 500, \"Sync\": false");
-			break;
-      }
-#ifdef DEBUG
-   	  Serial.print(", \"Packet\": \"0x");
-   	  Serial.print((long unsigned int) p, HEX);
-	  Serial.print("\"");
-#endif
-	  Serial.println("}");
-    }
-  }
+			JsonObject& root = jsonBuffer.createObject();
+			root["device"] = "weather";
+			root["id"] = station1.getId(p);
+			root["ch"] = station1.getChannel(p);
+			root["batt"] = station1.getBattery(p);
+			root["temp"] = station1.getTemperature(p);
+			root["hum"] = station1.getHumidity(p);
+			root["raw"] = station1.getString(p);
 
+			root.printTo(output);
+		}
+	}
 
-  if(front.available()) {
-    uint64_t p = front.getPacket(); //getPacket clears the packet, we keep it to print it further ahead
-    if(front.change(p)) {
-      Serial.print("{\"id\": \"door\", \"code\": 200, \"change\": 1");
-      Serial.println("}");
-    } else {
-      Serial.print("{\"id\": \"door\", \"code\": 500");
-#ifdef DEBUG
-      Serial.print(", \"Packet\": \"0x");
-      Serial.print((long unsigned int) p, HEX);
-      Serial.println("\"}");
-#endif
-    }
-  }
+	if(station2.available()) {
+		if((p = station2.getPacket())) {
+			StaticJsonBuffer<160> jsonBuffer;
+
+			JsonObject& root = jsonBuffer.createObject();
+			root["device"] = "digoo";
+			root["id"] = station2.getId(p);
+			root["ch"] = station2.getChannel(p);
+			root["batt"] = station2.getBattery(p);
+			root["temp"] = station2.getTemperature(p);
+			root["hum"] = station2.getHumidity(p);
+			root["raw"] = station2.getString(p);
+
+			root.printTo(output);
+		}
+	}
+
+	if(remote.available()) {
+		if((p = remote.getPacket())) {
+			StaticJsonBuffer<160> jsonBuffer;
+
+			JsonObject& root = jsonBuffer.createObject();
+			root["device"] = "generic";
+			root["raw"] = remote.getString(p);
+
+			root.printTo(output);
+		}
+	}
+
+	if(output.length()) {
+		Serial.println(output);
+	}
 }
