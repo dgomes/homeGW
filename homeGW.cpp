a176bd7ab110251fbbf6a06fe5117b071da3b7ff
Panagiotis <maragelis@gmail.com>
2018-02-25 15:50:29 +0200

    Support for esp8266
    Adjust interrupts accordingly

--- homeGW.cpp
+++ homeGW.cpp
@@ -46,7 +46,14 @@ void HomeGW::registerPlugin(Plugin *p) {
 }
 
 bool HomeGW::setup(uint8_t pin) {
-  if(!(pin == 3 || pin == 2)) {
+  bool isESP = false;
+
+#if defined(ESP8266)
+  isESP = true;
+#endif
+
+  if (!isESP && (!(pin == 3 || pin == 2)))
+  {
     return false;
   }
 
@@ -54,8 +61,18 @@ bool HomeGW::setup(uint8_t pin) {
 
   pinMode(pin, INPUT);
   digitalWrite(pin, LOW);
+  uint8_t interuptPin;
 
-  attachInterrupt(pin-2, HomeGW::handleInterrupt, CHANGE); // 1 = PIN3
+  if (!isESP)
+  {
+    interuptPin = pin - 2;
+  }
+  else
+  {
+    interuptPin = pin;
+  }
+
+  attachInterrupt(interuptPin, HomeGW::handleInterrupt, CHANGE); // 1 = PIN3
 
   return true;
 }
